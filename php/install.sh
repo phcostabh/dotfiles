9fb9cb78dc252fbc72048da650a25d90d4dd201d
Philippe Santana Costa <philippescosta@gmail.com>
2013-10-30 23:23:38 -0200

    Add file to installing PHP through PHPBrew.

---
+++ php/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if test $(which php); then
+    echo " Installing PHPBrew."
+    echo "   Installing PHPBrew requirements."
+    logfile="/tmp/phpbrew-install.log"
+    sudo apt-get install autoconf automake curl build-essential \
+        libxslt1-dev re2c libxml2-dev php5-cli bison libbz2-dev --yes > $logfile
+    sudo apt-get build-dep php5 --yes >> $logfile
+
+    echo "   Installing PHPBrew."
+    curl -o /tmp/phpbrew https://raw.github.com/c9s/phpbrew/master/phpbrew > $logfile 2>&1
+    chmod +x /tmp/phpbrew
+    sudo cp /tmp/phpbrew /usr/bin/phpbrew
+
+    echo "   Setting up PHPBrew autoloading."
+    echo "" >> $HOME/.bashrc
+    echo "[[ -e \"$HOME/.phpbrew/bashrc\" ]] && . \"$HOME/.phpbrew/bashrc\"" >> $HOME/.bashrc
+fi
