beef68dc85f2188fd1fa4cc057895a7521a33efe
Philippe Santana Costa <philippescosta@gmail.com>
2013-10-30 23:29:14 -0200

    Fix condition to installing PHPBrew.

--- php/install.sh
+++ php/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if test $(which php); then
+if test ! $(which php); then
     echo " Installing PHPBrew."
     echo "   Installing PHPBrew requirements."
     logfile="/tmp/phpbrew-install.log"
@@ -9,7 +9,7 @@ if test $(which php); then
     sudo apt-get build-dep php5 --yes >> $logfile
 
     echo "   Installing PHPBrew."
-    curl -o /tmp/phpbrew https://raw.github.com/c9s/phpbrew/master/phpbrew > $logfile 2>&1
+    curl -o /tmp/phpbrew https://raw.github.com/c9s/phpbrew/master/phpbrew >> $logfile 2>&1
     chmod +x /tmp/phpbrew
     sudo cp /tmp/phpbrew /usr/bin/phpbrew
 
