b35b9bdcc0e24f15a5abbf1cf89c32ec188b723f
Philippe Santana Costa <philippescosta@gmail.com>
2015-05-28 18:06:34 -0300

    reload yas-snippet on start up.

--- emacs/init-local-config.el
+++ emacs/init-local-config.el
@@ -52,5 +52,7 @@
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 
+(yas-reload-all) ;; Reload yas-snippet
+
 (provide 'init-local-config)
 ;;; init-local-config ends here
